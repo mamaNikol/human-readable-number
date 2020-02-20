5e6564e2c2d10886d5ad9f1d79fa7763394ab66f
mamaNikol <49046890+mamaNikol@users.noreply.github.com>
2020-02-20 12:15:12 +0800

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function toReadable (number) {
+    const numBeforetwelve = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+	 const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+if (number === 0){
+	return 'zero';
+}
+if(number < 20){
+	return numBeforetwelve[number];
+}
+if (number < 100){
+ return (tens[Math.floor(number / 10)] + ' ' 
+                             + numBeforetwelve[number % 10])
+}
+
   
 }
